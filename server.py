e072f4db75717d14abd34bf3ac34545ec050f97e
Sean Candon <candons@tcd.ie>
2019-02-06 22:53:06 +0000

    some progress made with https

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import ssl
 from thread import *
 
 PORT = 8080
@@ -37,9 +38,13 @@ def main():
 
 
 def conn_string(conn, data, addr):
+    https = False
     try:
         print data
         line1 = data.split('\n')[0]
+        get = line1.find("GET")
+        if get == -1:
+            https = True
         url = line1.split(' ')[1]
         http = url.find("://")
         if (http == -1):
@@ -59,14 +64,28 @@ def conn_string(conn, data, addr):
         else:
             port = int((rest[(port_pos+1):])[:server_pos-port_pos-1])
             webserver = rest[:port_pos]
-        #print "webserver = %s" % (webserver)
-        proxy_server(webserver, port, conn, data, addr)
+
+        lines = data.split('\n')
+        print len(lines)
+
+        host = ""
+        for l in lines:
+            h = l.find("Host")
+            if h != -1:
+                host = l.split(' ')[1]
+                break
+
+        if https is False:
+            proxy_server_http(webserver, port, conn, data, addr)
+        else:
+            proxy_server_https(webserver, port, conn, addr, host, url)
+
     except Exception, e:
         print "fuck didn't work 2"
         pass
 
 
-def proxy_server(webserver, port, conn, data, addr):
+def proxy_server_http(webserver, port, conn, data, addr):
     try:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.connect((webserver, port))
@@ -80,9 +99,9 @@ def proxy_server(webserver, port, conn, data, addr):
                 dar = float(dar / 1024)
                 dar = "%.3s" % (str(dar))
                 dar = "%s KB" % (dar)
-                #print "request complete: %s => %s <=" % (str(addr[0]), str(dar))
+                # print "request complete: %s => %s <=" % (str(addr[0]), str(dar))
             else:
-                #print "welp no reply"
+                # print "welp no reply"
                 break
 
         sock.close()
@@ -90,6 +109,31 @@ def proxy_server(webserver, port, conn, data, addr):
     except socket.error, (value, message):
         print "fuck didn't work 3"
         sock.close()
+        conn.close()
+        sys.exit(2)
+
+
+def proxy_server_https(webserver, port, conn, addr, host, url):
+    try:
+        context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+        context.verify_mode = ssl.CERT_NONE
+        context.check_hostname = False
+        conn2 = context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=webserver)
+        conn2.connect((webserver, 443))
+        conn2.sendall("GET "+url+" HTTP/1.1\r\n"+"Host: "+host+"\r\n"+"Connection: close\r\n"+"\r\n")
+
+        reply = ""
+        while 1:
+            temp = conn2.recv(1024)
+            if not temp:
+                break
+            reply += temp
+
+        conn.send(reply)
+        conn.close()
+        conn2.close()
+    except socket.error, (value, message):
+        print "fuck didn't work 4"
         conn.close()
         sys.exit(2)
 
